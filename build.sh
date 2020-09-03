55a8ea783d7aa47e0c977aec675baa274bfc6ac2
Pierson Brannan <plbrannan95@gmail.com>
2020-09-03 15:50:34 -0400

    Handle build copying of images

--- build.sh
+++ build.sh
@@ -1,4 +1,10 @@
+echo "Creating files..."
+mkdir -p ./dist/img
+
 echo "Copying ./src/index.html into dist/ ..."
-cp src/index.html dist
+cp ./src/index.html dist
+
+echo "Copying images into ./img/ ..."
+cp -r ./src/img/ dist/
 
 echo "Done"
